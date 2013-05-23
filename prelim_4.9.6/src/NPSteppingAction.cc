6f10118c0dc438c77be471854a76c30f5fd86f3b
nepahwin <james.loach@gmail.com>
2013-05-23 17:52:40 +0800

    Update so that output actually makes sense

--- prelim_4.9.6/src/NPSteppingAction.cc
+++ prelim_4.9.6/src/NPSteppingAction.cc
@@ -1,5 +1,9 @@
 
 #include "NPSteppingAction.hh"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -15,8 +19,8 @@ NPSteppingAction::NPSteppingAction() {
 
 NPSteppingAction::~NPSteppingAction() {
 
-  if(fFile) {
-    if(fDetectorE>0 || fTEPIDs.size()>0 || fDEPIDs.size()>0) fTree->Fill();
+  if ( fFile ) {
+    if ( fTEPIDs.size()>0 || fDEPIDs.size()>0) fTree->Fill();
     fTree->Write();
     fFile->Close();
   }
@@ -28,7 +32,7 @@ NPSteppingAction::~NPSteppingAction() {
 
 void NPSteppingAction::SetNewValue(G4UIcommand *command, G4String newValues) {
 
-  if(command == fFileNameCmd) fFileName = newValues;
+  if ( command == fFileNameCmd ) fFileName = newValues;
 
 }
 
@@ -36,7 +40,6 @@ void NPSteppingAction::SetNewValue(G4UIcommand *command, G4String newValues) {
 
 void NPSteppingAction::ResetVars() {
 
-  fDetectorE = 0;
   fTEPIDs.clear();
   fTEEnergies.clear();
   fTEMomentumCosTheta.clear();
@@ -49,74 +52,71 @@ void NPSteppingAction::ResetVars() {
 
 void NPSteppingAction::UserSteppingAction(const G4Step *step) {
 
-  if(fFile == NULL) {
+  if ( fFile == NULL ) {     
+
     fNEvents 
       = G4RunManager::GetRunManager()->GetCurrentRun()->GetNumberOfEventToBeProcessed();
-    if(fFileName == "") {
+    if ( fFileName == "" ) {
       G4int muE_GeV_AsInt 
         = (G4int) rint(step->GetPreStepPoint()->GetKineticEnergy()/GeV);
       fFile 
         = TFile::Open(TString::Format("nuproblm_%dGeV.root", muE_GeV_AsInt), "recreate");
-    }
-    else fFile = TFile::Open(fFileName.c_str(), "recreate");
+    } else fFile = TFile::Open(fFileName.c_str(), "recreate");
     fTree = new TTree("tree", "tree");
     fTree->Branch("nEvents", &fNEvents, "N/I");
-    fTree->Branch("detectorE_MeV", &fDetectorE, "E/D");
     fTree->Branch("tePID", &fTEPIDs);
     fTree->Branch("teE", &fTEEnergies);
     fTree->Branch("tePCosQ", &fTEMomentumCosTheta);
     fTree->Branch("dePID", &fDEPIDs);
     fTree->Branch("deE", &fDEEnergies);
     ResetVars();
-  }
-  else {
+
+  }  else {
+
     G4int eventID 
       = G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID();
     static G4int lastEventID = eventID;
-    if(eventID != lastEventID) {
-      if(fDetectorE>0 || fTEPIDs.size()>0 || fDEPIDs.size()>0) fTree->Fill();
+    if ( eventID != lastEventID ) {
+      if ( fTEPIDs.size() > 0 || fDEPIDs.size() > 0 ) fTree->Fill();
       ResetVars();
       lastEventID = eventID;
     }
+
   }
 
-  G4double energyDep = step->GetTotalEnergyDeposit();
-  fDetectorE += energyDep/MeV;
-
-  G4VPhysicalVolume* preStepVol = step->GetPreStepPoint()->GetPhysicalVolume();
+  G4VPhysicalVolume* preStepVol  = step->GetPreStepPoint()->GetPhysicalVolume();
   G4VPhysicalVolume* postStepVol = step->GetPostStepPoint()->GetPhysicalVolume();
-  if(preStepVol != postStepVol) {
-    static G4VPhysicalVolume* world = NULL;
-    static G4VPhysicalVolume* target = NULL;
-    static G4VPhysicalVolume* detector = NULL;
-    if(preStepVol != world && preStepVol != target && preStepVol != detector) {
-      if(world == NULL && preStepVol->GetName() == "world") world = preStepVol;
-      else if(target == NULL && preStepVol->GetName() == "target") target = preStepVol;
-      else if(detector == NULL && preStepVol->GetName() == "detector") detector = preStepVol;
-    }
-    if(postStepVol != world && postStepVol != target && postStepVol != detector) {
-      if(world == NULL && postStepVol->GetName() == "world") world = postStepVol;
-      else if(target == NULL && postStepVol->GetName() == "target") target = postStepVol;
-      else if(detector == NULL && postStepVol->GetName() == "detector") detector = postStepVol;
+
+  if ( preStepVol != NULL && postStepVol != NULL ) {
+
+    if ( preStepVol->GetName() == "target" && 
+         postStepVol->GetName() == "world" ) {
+
+      fTEPIDs.push_back(
+        step->GetTrack()->GetParticleDefinition()->GetPDGEncoding());
+      fTEEnergies.push_back(
+        step->GetTrack()->GetKineticEnergy()/MeV);
+      fTEMomentumCosTheta.push_back(
+          step->GetTrack()->GetMomentumDirection().cosTheta());
+      fDEPIDs.push_back(-999);
+      fDEEnergies.push_back(-999);
+
+    } else if ( preStepVol->GetName() == "world" &&
+                postStepVol->GetName() == "detector" ) {
+
+      fTEPIDs.push_back(-999);
+      fTEEnergies.push_back(-999);
+      fTEMomentumCosTheta.push_back(-999);
+      fDEPIDs.push_back(
+        step->GetTrack()->GetParticleDefinition()->GetPDGEncoding());
+      fDEEnergies.push_back(
+        step->GetTrack()->GetKineticEnergy()/MeV);
+
     }
 
-    // target exiters
-    if(preStepVol == target && postStepVol == world) {
-      fTEPIDs.push_back(step->GetTrack()->GetParticleDefinition()->GetPDGEncoding());
-      fTEEnergies.push_back(step->GetTrack()->GetKineticEnergy()/MeV);
-      fTEMomentumCosTheta.push_back(step->GetTrack()->GetMomentumDirection().cosTheta());
-    }
-    // detector enterers
-    else if(preStepVol == world && postStepVol == detector) {
-      fDEPIDs.push_back(step->GetTrack()->GetParticleDefinition()->GetPDGEncoding());
-      fDEEnergies.push_back(step->GetTrack()->GetKineticEnergy()/MeV);
-    }
   }
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-
-
-
 
